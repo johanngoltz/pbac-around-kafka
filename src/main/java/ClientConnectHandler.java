b3d8e9c81a81bb4e0c06824c0f44465d3fa6c460
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2022-04-25 11:10:03 +0200

    works

---
+++ src/main/java/ClientConnectHandler.java
@@ -0,0 +1,42 @@
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+@RequiredArgsConstructor
+public class ClientConnectHandler implements CompletionHandler<AsynchronousSocketChannel, Void> {
+	private final AsynchronousServerSocketChannel serverChannel;
+	private final AsynchronousSocketChannel kafkaChannel;
+	private final Purposes purposes;
+
+
+	/**
+	 * Invoked when an operation has completed.
+	 *
+	 * @param clientChannel The result of the I/O operation.
+	 * @param attachment
+	 */
+	@Override
+	public void completed(AsynchronousSocketChannel clientChannel, Void attachment) {
+		if (serverChannel.isOpen()) serverChannel.accept(null, this);
+
+		if (clientChannel != null && clientChannel.isOpen()) {
+			final var buffer = ByteBuffer.allocate(4096);
+			clientChannel.read(buffer, buffer, new ClientReceiveHandler(clientChannel, kafkaChannel, purposes));
+		}
+	}
+
+	/**
+	 * Invoked when an operation fails.
+	 *
+	 * @param exc        The exception to indicate why the I/O operation failed
+	 * @param attachment
+	 */
+	@Override
+	public void failed(Throwable exc, Void attachment) {
+
+	}
+}
---
+++ src/main/java/Purposes.java
@@ -0,0 +1,19 @@
+import org.apache.kafka.common.protocol.ApiKeys;
+import org.apache.kafka.common.requests.AbstractResponse;
+import org.apache.kafka.common.requests.RequestHeader;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Purposes {
+
+	private final Collection<ApiKeys> relevantApiKeys = List.of(ApiKeys.FETCH, ApiKeys.OFFSET_FETCH);
+
+	boolean isRequestPurposeRelevant(RequestHeader requestHeader) {
+		return relevantApiKeys.contains(requestHeader.apiKey());
+	}
+
+	AbstractResponse makeResponsePurposeCompliant(AbstractResponse response) {
+		return response;
+	}
+}
