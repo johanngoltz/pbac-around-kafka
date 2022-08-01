b99c6336608a04d2b577f9773fc61389e4bcb80f
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2022-08-01 18:54:45 +0200

    JQ: cache compiled queries

--- src/main/java/purposeawarekafka/jq.java
+++ src/main/java/purposeawarekafka/jq.java
@@ -3,6 +3,7 @@ package purposeawarekafka;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import net.thisptr.jackson.jq.BuiltinFunctionLoader;
 import net.thisptr.jackson.jq.JsonQuery;
 import net.thisptr.jackson.jq.Scope;
@@ -11,11 +12,17 @@ import org.apache.commons.io.input.CharSequenceReader;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class jq {
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private final Scope rootScope = Scope.newEmptyScope();
+	private final Scope scope = Scope.newChildScope(rootScope);
+
+	private final Map<String, JsonQuery> queries = Collections.synchronizedMap(new HashMap<>());
 
 	public jq() {
 		BuiltinFunctionLoader.getInstance().loadFunctions(Versions.JQ_1_6, rootScope);
@@ -31,12 +38,19 @@ public class jq {
 		}
 	}
 
+	@SneakyThrows
+	private JsonQuery compileQuery(String query) {
+		return JsonQuery.compile(query, Versions.JQ_1_6);
+	}
+
 	public JsonNode evaluate(String jqFilter, Reader json) throws IOException {
-		final var childScope = Scope.newChildScope(rootScope);
-		final var query = JsonQuery.compile(jqFilter, Versions.JQ_1_6);
-		final var tree = objectMapper.readTree(json);
 		final var results = new LinkedList<JsonNode>();
-		query.apply(childScope, tree, results::add);
+
+		final var query = queries.computeIfAbsent(jqFilter, this::compileQuery);
+		final var tree = objectMapper.readTree(json);
+
+		query.apply(scope, tree, results::add);
+
 		return results.get(0);
 	}
 }
