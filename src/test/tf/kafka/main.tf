486170f0117a8a9cc5b83db5069ce115fcff5970
Johann Goltz <johann.goltz@campus.tu-berlin.de>
2022-08-01 09:50:55 +0200

    remove unneeded declarations

--- src/test/tf/kafka/main.tf
+++ src/test/tf/kafka/main.tf
@@ -71,92 +71,6 @@ locals {
   ]
 }
 
-module "gce-container-kafka" {
-  count   = length(local.kafka_hosts)
-  source  = "terraform-google-modules/container-vm/google"
-  version = "~> 3.0"
-
-  container = {
-    image = "docker.io/bitnami/kafka:3.1"
-    env   = [
-      {
-        name  = "KAFKA_CFG_ZOOKEEPER_CONNECT"
-        value = "${google_compute_instance.zookeeper.network_interface[0].network_ip}:2181"
-      },
-      {
-        name  = "ALLOW_PLAINTEXT_LISTENER"
-        value = "yes"
-      },
-      {
-        name  = "KAFKA_CFG_LISTENERS"
-        value = "CLIENT://:29092,EXTERNAL://:9092"
-      },
-      {
-        name  = "KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP"
-        value = "CLIENT:PLAINTEXT,EXTERNAL:PLAINTEXT"
-      },
-      {
-        name  = "KAFKA_CFG_ADVERTISED_LISTENERS"
-        value = "CLIENT://${local.kafka_hosts[count.index]}:29092,EXTERNAL://%{if var.enable_pbac}pbac-${count.index}:9093%{else}${local.kafka_hosts[count.index]}:9092%{endif}"
-      },
-      {
-        name  = "KAFKA_CFG_INTER_BROKER_LISTENER_NAME"
-        value = "CLIENT"
-      },
-      {
-        name  = "KAFKA_CFG_LOG4J_ROOT_LOGLEVEL"
-        value = "TRACE"
-      },
-      {
-        name  = "KAFKA_CFG_LOG4J_LOGGERS"
-        value = "kafka=TRACE,kafka.request.logger=TRACE,org.apache.kafka=TRACE"
-      }, {
-        name  = "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR"
-        value = "1"
-      }
-    ]
-  }
-
-  restart_policy = "never"
-}
-
-module "gce-container-pbac" {
-  source  = "terraform-google-modules/container-vm/google"
-  version = "~> 3.0"
-
-  container = {
-    image = "us-central1-docker.pkg.dev/pbac-in-pubsub/the-repo/pbac-around-kafka:latest"
-    env   = [
-      {
-        name  = "PBAC_KAFKA_HOST"
-        value = "kafka-0"
-      },
-      {
-        name  = "PBAC_KAFKA_PORT"
-        value = "9092"
-      },
-      {
-        name  = "PBAC_NUM_THREADS"
-        value = "100"
-      },
-      {
-        name  = "PBAC_MODE"
-        value = "FILTER_ON_PUBLISH"
-      },
-      {
-        name  = "KAFKA_CFG_LOG4J_ROOT_LOGLEVEL"
-        value = "TRACE"
-      },
-      {
-        name  = "KAFKA_CFG_LOG4J_LOGGERS"
-        value = "kafka=TRACE,kafka.request.logger=TRACE,org.apache.kafka=TRACE"
-      }
-    ]
-  }
-
-  restart_policy = "never"
-}
-
 module "gce-container-zookeeper" {
   source  = "terraform-google-modules/container-vm/google"
   version = "~> 3.0"
@@ -255,42 +169,6 @@ resource "google_compute_instance" "kafka" {
     EOT
     google-logging-enabled     = "true"
     google-monitoring-enabled  = "true"
-  }
-
-  service_account {
-    email  = "terraform-local@pbac-in-pubsub.iam.gserviceaccount.com"
-    scopes = ["cloud-platform"]
-  }
-}
-
-resource "google_compute_instance" "pbac" {
-  count = 0 #var.enable_pbac ? length(local.kafka_hosts) : 0
-
-  name         = "pbac-${count.index}"
-  machine_type = "e2-medium"
-
-  boot_disk {
-    initialize_params {
-      image = module.gce-container-pbac.source_image
-    }
-  }
-
-  network_interface {
-    subnetwork = var.subnetwork.name
-    # access_config {}
-  }
-
-  tags = ["pbac", "benchmark"]
-
-  metadata = {
-    serial-port-logging-enable = "TRUE"
-    gce-container-declaration  = module.gce-container-pbac.metadata_value
-    google-logging-enabled     = "true"
-    google-monitoring-enabled  = "true"
-  }
-
-  labels = {
-    container-vm = module.gce-container-pbac.vm_container_label
   }
 
   service_account {
